ed7fbd15905090dd4e7820907210918863bbb6b7
Kacper Marcisz <kmarcisz@gmail.com>
2015-10-11 15:20:17 -0500

    projects description updates, css fixes

--- application.rb
+++ application.rb
@@ -2,8 +2,11 @@ require "sinatra"
 require "haml"
 require "nori"
 
+before do
+  @projects = Nori.new.parse(File.open("projects.xml", "rb").read)["projects"]["project"]
+  puts @projects
+end
+
 get "/" do
-  @projects = Nori.new.parse(File.open("projects.xml", "rb").read)
-  puts @projects.inspect
   haml :index
 end
